4f7fd0b79c25a1e06808e5fe632bac74940763e6
tchin94 <tchin94@users.noreply.github.com>
2025-09-29 00:38:22 -0700

    checkpoint

--- player.gd
+++ player.gd
@@ -1,5 +1,9 @@
 extends CharacterBody2D
 
+signal health_depleted
+
+var health = 100.0
+
 func _physics_process(delta):
 	const SPEED = 600
 	var direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
@@ -9,3 +13,12 @@ func _physics_process(delta):
 		%HappyBoo.play_walk_animation()
 	else:
 		%HappyBoo.play_idle_animation()
+
+	const DAMAGE_RATE = 5.0
+	var overlapping_mobs = %HurtBox.get_overlapping_bodies()
+	
+	if overlapping_mobs.size() > 0:
+		health -= overlapping_mobs * DAMAGE_RATE * delta
+		%ProgressBar.value = health
+		if health <= 0.0:
+			health_depleted.emit()
